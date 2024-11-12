5dea0bcee24e8a0d3f6e8c35ee4d8d221b57173c
Khazixi <khazixfangirl@gmail.com>
2024-11-12 14:02:23 -0500

    Refactored to use submit event and form data instead of individual values

--- front-end/src/components/ActivityFormComponent/ActivityFormComponent.js
+++ front-end/src/components/ActivityFormComponent/ActivityFormComponent.js
@@ -5,18 +5,18 @@ import { Events } from "../../eventhub/Events.js";
 export class ActivityFormComponent extends BaseComponent {
   #container = null;
 
-  constructor(){
+  constructor() {
     super();
     this.loadCSS('ActivityFormComponent');
   }
 
-  render(){
+  render() {
     this.#createContainer();
     this.#attachEventListeners();
     return this.#container;
   }
 
-  #createContainer(){
+  #createContainer() {
     this.#container = document.createElement('div');
     this.#container.classList.add('activity-form');
     this.#container.innerHTML = this.#getTemplate();
@@ -28,7 +28,7 @@ export class ActivityFormComponent extends BaseComponent {
         <h2>Add New Activity</h2>
 
         <label for="specific-day">Must happen on specific day:</label>
-        <select id="specific-day">
+        <select id="specific-day" name="specificDay">
           <option value="no">No</option>
           <option value="yes">Yes</option>
         </select>
@@ -36,7 +36,7 @@ export class ActivityFormComponent extends BaseComponent {
 
         <div id="choose-day" style="display: none">
           <label for="day-selection">Select day:</label>
-          <select id="day-selection">
+          <select id="day-selection" name="daySelection">
             <option value="day-1">Day 1</option>
             <option value="day-2">Day 2</option>
             <option value="day-3">Day 3</option>
@@ -45,25 +45,25 @@ export class ActivityFormComponent extends BaseComponent {
         </div>
 
         <label for="location">Name of location:</label>
-        <input type="text" id="location" placeholder="Enter location">
+        <input type="text" id="location" name="location" placeholder="Enter location">
         <br>
 
         <label for="address">Address:</label>
-        <input type="text" id="address">
+        <input type="text" id="address" name="address">
         <br>
 
         <label for="duration">Duration:</label>
-        <input type="text" id="duration" class="time-input">
+        <input type="text" id="duration" name="duration" class="time-input">
         <br>
 
         <label for="open-time">Opens at:</label>
-        <input type="text" id="open-time" class="time-input">
+        <input type="text" id="open-time" name="openTime" class="time-input">
         <label for="close-time">Closes at:</label>
-        <input type="text" id="close-time" class="time-input">
+        <input type="text" id="close-time" name="closeTime" class="time-input">
         <br>
 
         <label for="notes">Notes:</label>
-        <textarea id="notes"></textarea>
+        <textarea id="notes" name="notes"></textarea>
         <br>
 
         <button id="add-activity">Add Activity</button>
@@ -72,7 +72,7 @@ export class ActivityFormComponent extends BaseComponent {
     `;
   }
 
-  #attachEventListeners(){
+  #attachEventListeners() {
     const specificDay = this.#container.querySelector('#specific-day');
     const daySelection = this.#container.querySelector('#day-selection');
     const location = this.#container.querySelector('#location');
@@ -84,37 +84,47 @@ export class ActivityFormComponent extends BaseComponent {
     const addActivityBtn = this.#container.querySelector('#add-activity');
     const clearActivityBtn = this.#container.querySelector('#clear');
 
-    addActivityBtn.addEventListener('click', (e) => {
-      e.preventDefault();
-      this.#handleAddActivity(specificDay, daySelection, location, address, duration, openTime, closeTime, notes);
-    });
+    /** @type HTMLFormElement */
+    const submissionForm = this.#container.querySelector('form')
 
     clearActivityBtn.addEventListener('click', (e) => {
       e.preventDefault();
       this.#clearInputs();
     });
 
+    submissionForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const formData = new FormData(e.target)
+      this.#handleAddActivity2(formData)
+    })
+
     const hub = EventHub.getInstance();
     hub.subscribe('EditActivity', activityData => this.#fillFormEditActivity(activityData));
   }
 
-  #handleAddActivity(specificDay, day, location, address, duration, openTime, closeTime, notes){
-    this.#publishNewActivity(specificDay.value, day.value, location.value, address.value, duration.value, openTime.value, closeTime.value, notes.value)
+  /**
+    * @param {FormData} formData  - Data from the form in formData format
+    */
+  #handleAddActivity2(formData) {
+    this.#publishNewActivity2(Object.fromEntries(formData))
 
-    this.#clearInputs(specificDay, day, location, address, duration, openTime, closeTime, notes);
+    this.#clearInputs()
   }
 
-  #publishNewActivity(specificDay, day, location, address, duration, openTime, closeTime, notes){
+  /**
+    * @param {Object} data 
+    */
+  #publishNewActivity2(data) {
     const hub = EventHub.getInstance();
-    hub.publish(Events.NewActivity, { specificDay, day, location, address, duration, openTime, closeTime, notes });
-    hub.publish(Events.StoreActivity, { specificDay, day, location, address, duration, openTime, closeTime, notes });
+    hub.publish(Events.NewActivity, data);
+    hub.publish(Events.StoreActivity, data);
   }
 
-  #clearInputs(specificDay, day, location, address, duration, openTime, closeTime, notes){
+  #clearInputs() {
     this.#container.querySelector('form')?.reset()
   }
 
-  #fillFormEditActivity(activityData){
+  #fillFormEditActivity(activityData) {
     const data = activityData.activityData;
 
     this.#container.querySelector('#specific-day').value = data.specificDay;
