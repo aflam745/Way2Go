5d4873642e64c59420ab3062c3463c22a4a118fa
Andrew Flammia <aflam745@gmail.com>
2024-11-17 17:01:47 -0500

    added activity page component

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,13 +1,10 @@
-import { ActivityFormComponent } from "./components/ActivityFormComponent/ActivityFormComponent.js";
-import { ActivityListComponent } from "./components/ActivityListComponent/ActivityListComponent.js";
+import { ActivityPageComponent } from "./components/ActivityPageComponent/ActivityPageComponent.js";
 import DirectionsComponent from "./components/DirectionsComponent/DirectionsComponent.js";
 const app = document.getElementById('app');
 
-const activityForm = new ActivityFormComponent();
-const activityList = new ActivityListComponent();
+const activityPage = new ActivityPageComponent();
 
-app.appendChild(activityForm.render());
-app.appendChild(activityList.render());
+app.appendChild(activityPage.render());
 
 // Services
 const taskRepository = new DirectionsComponent();
