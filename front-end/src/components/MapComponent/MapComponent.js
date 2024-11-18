42a6fba92dc3adb227438278217296d986739eb6
jimmyjiang57 <jimmychjiang@gmail.com>
2024-11-17 23:28:27 -0500

    map component conflict

---
+++ front-end/src/components/MapComponent/MapComponent.js
@@ -0,0 +1,91 @@
+import { BaseComponent } from "../BaseComponent/BaseComponent.js";
+
+export class MapComponent extends BaseComponent {
+    #container = null;
+
+    constructor(activities, polyline=null) {
+        super();
+        this.activities = activities;
+        this.polyline = polyline;
+        this.map = null;
+    }
+
+    render() {
+        this.#createContainer();
+        this.#loadLeafletResources(() => this.#initializeMap());
+        return this.#container;
+    }
+
+    removeSelf() {
+        this.map.off();
+        this.map.remove();
+        this.map = null;
+    }
+
+    #createContainer() {
+        this.#container = document.createElement('div');
+        this.#container.style.height = '400px';
+        this.#container.style.width = '100%';
+        this.#container.id = 'map'; 
+    }
+
+    #loadLeafletResources(callback) {
+        const mapScript = document.createElement('script');
+        mapScript.src = "https://unpkg.com/leaflet/dist/leaflet.js";
+        mapScript.onload = callback.bind(this);
+        document.head.appendChild(mapScript);
+
+        this.loadCSS('MapComponent');
+        const mapStyles = document.createElement('link');
+        mapStyles.rel = 'stylesheet';
+        mapStyles.href = "https://unpkg.com/leaflet/dist/leaflet.css";
+        document.head.appendChild(mapStyles);
+    }
+
+    #initializeMap() {      
+        this.map = L.map(this.#container.id);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors',
+        }).addTo(this.map);
+
+        const markers = this.activities.map((activity, index) => {
+            const originalTooltipText = `${index + 1}. ${activity.name}`
+
+            // default tooltip
+            const marker = L.marker([activity.location.lat, activity.location.lon])
+                .bindTooltip(
+                    originalTooltipText,
+                    { permanent: true, direction: "right", className: "leaflet-label" }
+                )
+                .addTo(this.map);
+            
+            // tooltip hover effect
+            marker.on('mouseover', () => {
+                marker.unbindTooltip();
+                marker.bindTooltip(
+                    originalTooltipText + `<br><i>${activity.location.address}</i>`,
+                    { permanent: false, direction: "right", className: "leaflet-label" }
+                ).openTooltip();
+            });
+
+            // reset tooltip off hover
+            marker.on('mouseout', () => {
+                marker.unbindTooltip(); 
+                marker.bindTooltip(
+                    originalTooltipText,
+                    { permanent: true, direction: "right", className: "leaflet-label" }
+                );
+            });
+            
+            return marker;
+        });
+
+
+        if (markers.length > 0) {
+            const group = L.featureGroup(markers);
+            this.map.fitBounds(group.getBounds());
+        }
+
+        if (this.polyline) L.geoJSON(this.polyline).addTo(this.map);
+    }
+}
