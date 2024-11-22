229a12e4edc8c5f6b606098b672eaf5021754285
Khazixi <khazixfangirl@gmail.com>
2024-11-22 12:34:13 -0500

    Added Layout Support

--- front-end/src/components/RouterComponent/RouterComponent.js
+++ front-end/src/components/RouterComponent/RouterComponent.js
@@ -1,16 +1,19 @@
-import { ActivityPageComponent } from "../ActivityPageComponent/ActivityPageComponent.js";
 import { BaseComponent } from "../BaseComponent/BaseComponent.js";
 
 // TODO: String manipulation for URL?
 
 /**
   * Router Component binds to a rootContainer and Renders
-  * pages based on the URL state that is provided to it
+  * pages based on the URL state that is provided to it.
+  * The router is static so router parameters do not work.
   */
 export class RouterComponent extends BaseComponent {
-  /** @param {HTMLElement} rootContainer - Root conatiner */
-  /** @param {Map<string, BaseComponent>} routes  */
-  constructor(rootContainer, routes) {
+  /** 
+    * @param {HTMLElement} rootContainer - Root conatiner 
+    * @param {Map<string, BaseComponent>} routes
+    * @param {BaseComponent?} layout
+    */
+  constructor(rootContainer, routes, layout = null) {
     super()
     /** 
       * @type {Element}
@@ -20,6 +23,8 @@ export class RouterComponent extends BaseComponent {
 
     /** @type {Map<string, BaseComponent>} */
     this.routes = routes
+
+    this.layout = layout
 
     if (!this.routes.has('/')) {
       throw Error("Must contain a route with pattern '/'")
@@ -40,16 +45,24 @@ export class RouterComponent extends BaseComponent {
     * @param {Location | URL} url 
     */
   render(url) {
-    // Before rendering the new page the old page needs to be cleared
-    // this.conatiner.replaceChildren()
-
     // Match based on Regex?
+
     /** @type {BaseComponent} */
     const component = this.routes.get(url.pathname) ?? this.routes.get('/')
 
-    this.conatiner.replaceChildren(
-      component.render()
-    )
-  }
 
+    if (this.layout != null) {
+      this.conatiner.replaceChildren(
+        this.layout.render(
+          component.render()
+        )
+      )
+    } else {
+      this.conatiner.replaceChildren(
+        component.render()
+      )
+    }
+
+    return this.conatiner
+  }
 }
