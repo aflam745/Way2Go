81d7f77f7f4f0b358b0acc933f79531f466371b8
robert winfield <winfieldrl@gmail.com>
2024-11-24 19:57:55 -0500

    Broke up into smaller functions - Improved logical flow - Fixed edge cases where input field is empty - Added more descriptive comments - Added styling

--- front-end/src/components/LocationSearchComponent/LocationSearchComponent.js
+++ front-end/src/components/LocationSearchComponent/LocationSearchComponent.js
@@ -1,18 +1,39 @@
-import { EventHub } from "../../eventhub/EventHub";
-import { Events } from "../../eventhub/Events";
+import { BaseComponent } from "../BaseComponent/BaseComponent.js"
 
-export default class LocationSearchComponent {
+export default class LocationSearchComponent extends BaseComponent {
     #container = null;
-    #addressDisplay = null;
-    #searchWrapper = null;
-    #searchForm = null;
-    #searchLabel = null;
     #searchInput = null;
     #searchResults = null;
+    #timeoutId = null;
+    #location = null;
 
+    constructor() {
+        super();
+        this.loadCSS("LocationSearchComponent");
+        this.#initLocation();
+    }
 
-    constructor () {
+    /**
+     * Provides the entered location.
+     * @returns {{address: string, lat: number, lon: number}} An object containing the location details.
+     */
+    getLocation() {
+        return { ...this.#location };
+    }
 
+    #initLocation() {
+        this.#location = {
+            lon: null,
+            lat: null,
+            address: null,
+        };
+    }
+
+    render() {
+        this.#initContainer();
+        this.#createSearchBar();
+        this.#addEventListeners();
+        return this.#container;
     }
 
     #initContainer() {
@@ -21,117 +42,126 @@ export default class LocationSearchComponent {
         this.#container.id = 'locationSearchComponent';
     }
 
-    render() {
-        this.#initContainer();
-        this.#createSearchBar();
-        this.#initEntryEventListener();
-        // this.#createAddressDisplay();
-    }
-
-    // #createAddressDisplay() {
-    //     this.#addressDisplay = document.createElement('div');
-    //     this.#addressDisplay.classList.add('address-display');
-    //     this.#addressDisplay.id = 'addressDisplay';
-    //     this.#addressDisplay.innerText = '';
-        
-    // }
-
     #createSearchBar() {
-        // form wrapper for the search bar and label
-        this.#searchForm = document.createElement('form');
-        this.#searchForm.classList.add('search-form');
-        this.#searchForm.id = 'searchForm';
-
-        // "Search" label
-        this.#searchLabel = document.createElement('label');
-        this.#searchLabel.htmlFor('searchInput');
-        this.#searchLabel.id = 'search-label'
-        this.#searchLabel.innerText = "Search"
-        this.#searchForm.appendChild(this.#searchLabel);
-
         // search bar
         this.#searchInput = document.createElement('input');
         this.#searchInput.classList.add('search-input');
         this.#searchInput.id = 'searchInput';
-        this.#searchForm.appendChild(this.#searchForm);        
+        this.#searchInput.placeholder = 'Enter a location...';
+        this.#container.appendChild(this.#searchInput);
 
         // dropdown display of results
         this.#searchResults = document.createElement('div');
         this.#searchResults.classList.add('search-results');
         this.#searchResults.id = 'searchResults';
+        this.#container.appendChild(this.#searchResults);
     }
 
-    #initEntryEventListener() {
+    #addEventListeners() {
+        document.addEventListener("mousedown", this.#handleClickOutside.bind(this));
+        this.#searchInput.addEventListener("input", this.#handleInputChange.bind(this));
+    }
 
-        const searchResults = this.#searchResults;
-        const searchInput = this.#searchInput;
+    #handleClickOutside(event) {
+        // close the dropdown if the click is outside the input or dropdown
+        if (!this.#isClickInContainer(event)) {
+            // if the user has a partially entered (nonvalid) address,
+            // then it will reset the input field to the last entered address
+            this.#resetEntry();
+        } else if (this.#isClickInInputField(event)) {
+            // show the dropdown when clicking the search input
+            this.#displaySearchResults();
+        }
+    }
 
+    #handleInputChange(event) {
+        clearTimeout(this.#timeoutId); // clear any previous timeout
+        if (event.target.value === '') {
+            this.#searchResults.innerHTML = '';
+            this.#initLocation();
+            return;
+        }
+        this.#displaySearchingText(event);
+        this.#timeoutId = setTimeout(async () => {
+            const data = await this.#fetchLocations(event);
+            this.#searchResults.innerHTML = ''; // clear "Searching..."
 
+            if (data.length === 0) this.#displayNoResults();
 
-        // address.innerHTML = "Selected address: none"
+            data.forEach(place => {
+                // create dropdown entry for each result
+                this.#createDropdownElement(place);
+            });
+        }, 1500);
+    }
 
-        let location = {}; // Variable to store the selected location
+    #resetEntry() {
+        this.#searchResults.innerHTML = '';
+        this.#searchResults.style.display = 'none';
+        // if all text is removed, then also clear the saved address
+        if (this.#searchInput.value === '') {
+            this.#initLocation();
+        } else {
+            this.#searchInput.value = this.#location.address;
+        }
+    }
 
-        document.addEventListener('mousedown', function (event) {
-            // Close the dropdown if the click is outside the input or dropdown
-            if (!(searchResults.contains(event.target) || searchInput.contains(event.target))) {
-                searchResults.style.display = 'none';
-            } else if (searchInput.contains(event.target)) {
-                // Show the dropdown when clicking the search input
-                searchResults.style.display = 'block';
-            }
+    async #fetchLocations(event) {
+        // if there's no text, don't call the API
+        if (!event.target.value) return [];
+        const encodedEntry = encodeURIComponent(event.target.value);
+        const res = await fetch(`https://nominatim.openstreetmap.org/search?q=${encodedEntry}&format=json&polygon_kml=1&addressdetails=1`);
+        if (!res.ok) throw new Error(`Error fetching results: ${res.status} ${res.statusText}`);
+        const resjson = await res.json();
+        return resjson;
+    }
+
+    #displaySearchingText(event) {
+        const searching = document.createElement('a');
+        this.#searchResults.innerHTML = '';
+        if (event.target.value !== '')
+            searching.innerHTML = "<i>Searching...</i>";
+        else (searching.style.display = 'none');
+        this.#searchResults.appendChild(searching);
+    }
+
+    #displaySearchResults() {
+        this.#searchResults.style.display = 'block';
+    }
+
+    #displayNoResults() {
+        const noResults = document.createElement('a');
+        noResults.innerText = "No results found.";
+        this.#searchResults.appendChild(noResults);
+    }
+
+    #createDropdownElement(place) {
+        const item = document.createElement('a');
+        item.innerHTML = place.display_name;
+        item.style.cursor = 'pointer';
+        this.#searchResults.appendChild(item);
+
+        item.addEventListener('mouseup', () => {
+            this.#searchInput.value = place.display_name;
+
+            const location = {
+                address: place.display_name,
+                lat: place.lat,
+                lon: place.lon
+            };
+
+            this.#location = location;
+
+            this.#searchResults.innerHTML = '';
+            this.#searchResults.style.display = 'none';
         });
+    }
 
-        let timeoutId;
+    #isClickInContainer(event) {
+        return this.#container.contains(event.target);
+    }
 
-        searchInput.addEventListener('keyup', function (event) {
-            clearTimeout(timeoutId); // Clear any previous timeout
-
-            const searching = document.createElement('a');
-            searchResults.innerHTML = ''; // Clear old results
-            if (event.target.value !== '')
-                searching.innerHTML = "<i>Searching...</i>"; // Placeholder while loading
-            else (searching.style.display = 'none');
-            searchResults.appendChild(searching);
-
-            timeoutId = setTimeout(async () => {
-                const encodedEntry = encodeURIComponent(event.target.value);
-                const res = await fetch(`https://nominatim.openstreetmap.org/search?q=${encodedEntry}&format=json&polygon_kml=1&addressdetails=1`);
-                const data = await res.json();
-
-                searchResults.innerHTML = ''; // Clear placeholder
-
-                data.forEach(place => {
-                    // Create dropdown entry
-                    const item = document.createElement('a');
-                    item.innerHTML = place.display_name;
-                    item.style.cursor = 'pointer'
-                    searchResults.appendChild(item);
-
-                    // Add click listener for selection
-                    item.addEventListener('mousedown', () => {
-                        // Store selected location
-
-                        location = {
-                            address: place.display_name,
-                            lat: place.lat,
-                            lon: place.lon
-                        };
-
-                        EventHub.getInstance().publish(Events.LocationSelect, location);
-
-                        console.log('Selected Location:', location);
-
-                        searchResults.innerHTML = '';
-                        // Close the dropdown
-                        searchResults.style.display = 'none';
-
-                        // address.innerHTML = "Selected address: " + place.display_name;
-                        searchInput.value = "";
-                    });
-                });
-            }, 1500);
-        });
-        
+    #isClickInInputField(event) {
+        return this.#searchInput.contains(event.target);
     }
 }
